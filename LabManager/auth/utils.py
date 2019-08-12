7b0d2e217e5afbfd93900a6a33b56b0796e8fe64
Diego Garcia Cordeiro Souza <diego00alfa@gmail.com>
2019-08-12 20:07:28 -0300

    Started JWT functionality and CRUD ops on the Auth module.

--- LabManager/auth/utils.py
+++ LabManager/auth/utils.py
@@ -1,7 +1,11 @@
 import os
 import secrets
+import jwt
+from functools import wraps
 from PIL import Image
+from flask import request, jsonify
 from LabManager import app
+from LabManager.dbModels import User
 
 
 def save_profile_picture(form_picture, image_path):
@@ -27,3 +31,53 @@ def save_profile_picture(form_picture, image_path):
     output_pic.save(pic_path)
 
     return pic_filename, os.path.join("static/profile_pics", pic_filename)
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        invalid_msg = {
+            'message': 'Invalid token. Registration and / or authentication required.',
+            'authenticated': False
+        }
+        expired_msg = {
+            'message': 'Expired token. Reauthentication required.',
+            'authenticated': False
+        }
+        internals_msg = {
+            'message': 'Server-side error. Please seek shelter and contact support.',
+            'authenticated': False
+        }
+
+        auth_headers = request.headers.get("Authorization", "").split()
+        if len(auth_headers) != 2:
+            invalid_msg["in"] = "line 50"
+            return jsonify(invalid_msg), 401
+
+        try:
+            token = auth_headers[1]
+            data = jwt.decode(token, app.config["SECRET_KEY"])
+            id = int(data["id"])
+            current_user = User.query.filter_by(id=id).first()
+            if not current_user:
+                raise RuntimeError('User not found')
+
+            return f(current_user, *args, **kwargs)
+
+        except RuntimeError:
+            return jsonify(internals_msg), 500
+
+        except TypeError:
+            return jsonify(internals_msg), 500
+        
+        except jwt.ExpiredSignatureError:
+            return jsonify(expired_msg), 401
+
+        except (jwt.InvalidTokenError, Exception) as e:
+            print(e)
+            invalid_msg["in"] = "line 74"
+            invalid_msg["exception"] = str(e)
+            invalid_msg["retrieved token"] = token
+            invalid_msg["decoded token"] = data
+            return jsonify(invalid_msg), 401
+
+    return decorated
